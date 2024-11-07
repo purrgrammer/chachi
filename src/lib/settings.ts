d358dfca5e935a06593ffa46c7247f8712edf003
Alejandro Gómez <alejandrogomez@bitrefill.com>
2024-11-08 00:36:34 +0100

    fix: forgot to commit this file

---
+++ src/lib/settings.ts
@@ -0,0 +1,19 @@
+import { useAtomValue } from "jotai";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
+
+interface Settings {
+  devMode?: boolean;
+}
+
+export const settingsAtom = atomWithStorage<Settings>(
+  "settings",
+  {
+    devMode: false,
+  },
+  createJSONStorage<Settings>(() => localStorage),
+  { getOnInit: true },
+);
+
+export function useSettings() {
+  return useAtomValue(settingsAtom);
+}
