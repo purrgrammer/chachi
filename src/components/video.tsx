89655be89a4a4ef226ec7b44c019fe421eb83248
Alejandro Gómez <alejandrogomez@bitrefill.com>
2024-11-08 01:01:07 +0100

    fix: preload video metadata

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 export function Video({ url, className }: { url: string; className?: string }) {
   return (
     <video
+      preload="metadata"
       controls
       src={url}
       className={cn("bg-background aspect-video rounded-md", className)}
