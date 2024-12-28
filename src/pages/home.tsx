6b97fbc82385a5f1612e0b14c4bb13e160b4aa94
rasputin <ra5pvt1n@proton.me>
2024-12-28 10:06:49 +0100

    add translation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/header";
 import { Dashboard } from "@/components/dashboard";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation();
   return (
     <>
       <Header>
-        <h2 className="text-lg">Home</h2>
+        <h2 className="text-lg">{t("home")}</h2>
       </Header>
       <div className="flex flex-col p-2">
         <Dashboard />
