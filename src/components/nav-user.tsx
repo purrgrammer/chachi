c504fad4f4f4bfc2f57a1c0f196e8c41224e219c
rasputin <ra5pvt1n@proton.me>
2024-12-28 10:32:37 +0100

    add translation

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -30,16 +30,17 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar";
 import { useAccount, useLogout } from "@/lib/account";
+import { useTranslation } from "react-i18next";
 
 function UserInfo({ pubkey }: { pubkey: string }) {
   return (
     <>
-      <Avatar pubkey={pubkey} className="h-8 w-8 rounded-lg" />
-      <div className="grid flex-1 text-left text-sm leading-tight">
-        <span className="truncate font-semibold">
+      <Avatar pubkey={pubkey} className="w-8 h-8 rounded-lg" />
+      <div className="grid flex-1 text-sm leading-tight text-left">
+        <span className="font-semibold truncate">
           <Name pubkey={pubkey} />
         </span>
-        <span className="truncate text-xs">
+        <span className="text-xs truncate">
           <Nip05 pubkey={pubkey} />
         </span>
       </div>
@@ -54,7 +55,10 @@ export function NavUser() {
   const account = useAccount();
   const pubkey = account?.pubkey;
   const isExpanded = state === "expanded" || open || openMobile;
-
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <SidebarMenu>
       {pubkey ? (
@@ -70,36 +74,69 @@ export function NavUser() {
               </SidebarMenuButton>
             </DropdownMenuTrigger>
             <DropdownMenuContent
-              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              className="rounded-lg w-[--radix-dropdown-menu-trigger-width] min-w-56"
               side={isMobile ? "bottom" : "right"}
               align="end"
               sideOffset={4}
             >
               <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <div className="flex gap-2 items-center py-1.5 px-1 text-sm text-left">
                   <UserInfo pubkey={pubkey} />
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuSub>
                 <DropdownMenuSubTrigger>
-                  <span>Theme</span>
+                  <span>{t("nav.user.language")}</span>
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuItem onClick={() => changeLanguage("en")}>
+                      <span>English</span>
+                      {i18n.language === "en" ? (
+                        <Check className="ml-auto" />
+                      ) : null}
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => changeLanguage("es")}>
+                      <span>Español</span>
+                      {i18n.language === "es" ? (
+                        <Check className="ml-auto" />
+                      ) : null}
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => changeLanguage("hans")}>
+                      <span>简体中文</span>
+                      {i18n.language === "hans" ? (
+                        <Check className="ml-auto" />
+                      ) : null}
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => changeLanguage("hant")}>
+                      <span>繁體中文</span>
+                      {i18n.language === "hant" ? (
+                        <Check className="ml-auto" />
+                      ) : null}
+                    </DropdownMenuItem>
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  <span>{t("nav.user.theme.trigger")}</span>
                 </DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
                   <DropdownMenuSubContent>
                     <DropdownMenuItem onClick={() => setTheme("light")}>
                       <Sun />
-                      <span>Light</span>
+                      <span>{t("nav.user.theme.light")}</span>
                       {theme === "light" ? <Check className="ml-auto" /> : null}
                     </DropdownMenuItem>
                     <DropdownMenuItem onClick={() => setTheme("dark")}>
                       <Moon />
-                      <span>Dark</span>
+                      <span>{t("nav.user.theme.dark")}</span>
                       {theme === "dark" ? <Check className="ml-auto" /> : null}
                     </DropdownMenuItem>
                     <DropdownMenuItem onClick={() => setTheme("system")}>
                       <SunMoon />
-                      <span>System</span>
+                      <span>{t("nav.user.theme.system")}</span>
                       {theme === "system" ? (
                         <Check className="ml-auto" />
                       ) : null}
@@ -110,7 +147,7 @@ export function NavUser() {
               <DropdownMenuSeparator />
               <DropdownMenuItem onClick={logout}>
                 <LogOut />
-                Log out
+                {t("nav.user.logout")}
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
