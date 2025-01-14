157cb649b8ac780583ef85acc1a120b9b7545782
hzrd149 <github@hzrd149.com>
2025-01-14 08:23:54 -0600

    add minimal PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.png"],
+      manifest: {
+        name: "chachi",
+        short_name: "chachi",
+        description: "a mobile-friendly nostr group chat client",
+        theme_color: "#7c3aed",
+        icons: [
+          {
+            src: "favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          }
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3*1024*1024
+      },
+    }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
