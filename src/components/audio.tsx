297f85b7cdf5b78df45d9f6d7a330864b58cef92
Alejandro Gómez <alejandrogomez@bitrefill.com>
2025-03-24 12:12:52 +0100

    fix: audio width

--- src/components/audio.tsx
+++ src/components/audio.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 export function Audio({ url, className }: { url: string; className?: string }) {
-  return <audio controls src={url} className={className} />;
+  return <audio controls src={url} className={cn("w-full", className)} />;
 }
