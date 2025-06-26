aab13b3ae3bb1b251382dac8ea21f9fd425bc234
Alejandro Gómez <alejandrogomez@bitrefill.com>
2025-06-26 12:13:53 +0200

    fix: revert manual chunking

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -26,14 +26,6 @@ const FormFieldContext = React.createContext<FormFieldContextValue>(
   {} as FormFieldContextValue,
 );
 
-type FormItemContextValue = {
-  id: string;
-};
-
-const FormItemContext = React.createContext<FormItemContextValue>(
-  {} as FormItemContextValue,
-);
-
 const FormField = <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
@@ -69,6 +61,14 @@ const useFormField = () => {
     ...fieldState,
   };
 };
+
+type FormItemContextValue = {
+  id: string;
+};
+
+const FormItemContext = React.createContext<FormItemContextValue>(
+  {} as FormItemContextValue,
+);
 
 const FormItem = React.forwardRef<
   HTMLDivElement,
